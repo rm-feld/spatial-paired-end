b842ef2d7aa203ae1c700779c6bec60e63000ad4
rm-feld <rfeldmeier@utexas.edu>
2023-07-07 10:04:50 -0500

    clean notebook and faster reads

--- src/seqs_and_reads/consts.py
+++ src/seqs_and_reads/consts.py
@@ -4,6 +4,7 @@
 FRAGMENT_LENGTH = [50000, 1000000] # size of fragment being sequenced
 
 # MUTATION SPECIFICATIONS
+MAX_POPS = 7
 POP_MUT = [1, 2, 3] # number of distinct mutations added per population
 POP_MUT_WEIGHT = [0.2, 0.5, 0.3] # population frequencies
 MUT_LENGTH = [4000, 15000]
@@ -15,3 +16,6 @@ TOL = 20 # number of
 READ_SEP = [1000, 3000] # range of separations of reads
 READ_LEN = 150
 COVERAGE = 30
+
+# EVALUATION SPECS
+ETOL = 0.05 # frequency diff at which populations are considered 'the same'
--- src/seqs_and_reads/sequencing.py
+++ src/seqs_and_reads/sequencing.py
@@ -2,32 +2,44 @@ import numpy as np
 from .consts import * 
 
 def paired_end_reads(seq, prop):
+    """ generates paired end reads of sequence with intensity moderated by frequency
+    of population
+
+    Parameters
+    ----------
+    seq : Array
+        Array of integers representing position to which a read should be mapped
+        on reference genome 
+    prop : float
+        proportion of total population which this genome makes up
+    Returns
+    -------
+    coords : Array
+        n x 2 array of points
+    """    
     R0, R1 = READ_SEP
     # identify approximate number of reads we need to get expected coverage
-    n = (len(seq) * prop) // (2 * READ_LEN) * COVERAGE
+    n = int((len(seq) * prop) // (2 * READ_LEN) * COVERAGE)
     l = len(seq)
 
-    # initialize reads and ends
-    reads = []
-    coords = []
+    def choose_r1s(sep):
+        return np.random.randint(0, l - sep - 2 * READ_LEN - 1)
+    choose_r1s_v = np.vectorize(choose_r1s)
 
-    print("expected iterations:", n)
-    for i in range(n):
-        # choose separation randomly 
-        sep = np.random.randint(R0, R1)
-        # pick first read loc with above restriction
-        r1s = np.random.randint(0, l - sep - 2 * READ_LEN - 1)
-        r1e = r1s + READ_LEN
-        r2s = r1e + sep 
-        r2e = r2s + READ_LEN
-        reads.append([seq[r1s:r1e], seq[r2s:r2e]])
-        coords.append((seq[r1s], seq[r2e]))
-        if i % 40 == 0:
-            print(f"{n - i} iterations to go!")
+    # choose separation randomly
+    seps = np.random.randint(R0, R1, size = n)
+    r1s = choose_r1s_v(seps) # pick first read loc with above restriction
+    r1e = r1s + READ_LEN
+    r2s = r1e + seps
+    r2e = r2s + READ_LEN
+
+    # return coords as an n x 2 array of coordindate values
+    coords = np.asarray([seq[r1s], seq[r2e]]).T
     
-    return reads, coords
+    return coords
 
 def find_coverage(reads, ref):
+    #TODO: update to take only coords
     l = len(ref)
     reads = np.concatenate([np.concatenate(read).flat for read in reads])
     return np.bincount(reads, minlength = l)
